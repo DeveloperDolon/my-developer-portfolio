5c78ea2c128d2f4b180bee3aee91be2d5cd03797
Dolon Roy <dolonr718@gmail.com>
2023-12-11 17:19:32 +0600

    banner section added commit

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/utils/motion";
 import { SparklesIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroContent = () => {
   return (
@@ -18,30 +19,53 @@ const HeroContent = () => {
       className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20] relative"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
-        
         <motion.div
           variants={slideInFromTop}
           className="Welcome-box2 button-primary overflow-hidden border border-[#7042f88b] opacity-[0.2] shadow-2xl shadow-[#fafafa]"
         >
-          <Image
-            src="/sb.png"
-            alt="work icons"
-            height={300}
-            width={300}
-          />
+          <Image src="/sb.png" alt="work icons" height={300} width={300} />
         </motion.div>
 
-        <motion.div
+        {/* <motion.div
           variants={slideInFromTop}
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">Fullstack Developer</h1>
+        </motion.div> */}
+
+
+        <motion.div
+        variants={slideInFromLeft(0.8)} 
+        className="space-y-2"
+        >
+          <h1 className="md:text-3xl text-xl font-semibold text-white">Hello There👋</h1>
+          <h1 className="md:text-3xl text-xl font-semibold text-white">I am Dolon Chandra Roy.</h1>
+          <h1 className="md:text-3xl text-xl font-semibold text-white">I am a {" "}
+          <TypeAnimation
+            sequence={[
+              "Web Developer.",
+              2000,
+              "Frontend Developer.",
+              2000,
+              "ReactJS Developer.",
+              2000,
+              "MERN Stack Developer.",
+              2000,
+              "Full Stack Developer.",
+              2000,
+            ]}
+            wrapper="span"
+            speed={50}
+            className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"
+            repeat={Infinity}
+          />
+          </h1>
         </motion.div>
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6 mt-3 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
             Providing
@@ -52,10 +76,9 @@ const HeroContent = () => {
             project exprience
           </span>
         </motion.div>
-
         <motion.p
           variants={slideInFromLeft(0.8)}
-          className="text-lg text-gray-400 my-5 max-w-[600px]"
+          className="text-lg text-gray-400 my-3 max-w-[600px]"
         >
           I&apos;m a Full Stack Software Engineer with experience in Website,
           Mobile, and Software development. Check out my projects and skills.
